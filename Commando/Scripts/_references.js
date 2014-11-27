1d3cf42a456413f2d19104a25b9fff9ba09da457
Jake0303 <jake0303@hotmail.ca>
2014-11-27 13:40:26 -0500

    Fixed changes

--- Commando/Scripts/_references.js
+++ Commando/Scripts/_references.js
@@ -1,11 +1,43 @@
 ﻿/// <autosync enabled="true" />
-/// <reference path="gameobjects.ts" />
-/// <reference path="constants.ts" />
-/// <reference path="assets.ts" />
-/// <reference path="state.ts" />
+/// <reference path="filters/alphamapfilter.js" />
+/// <reference path="filters/alphamaskfilter.js" />
+/// <reference path="filters/boxblurfilter.js" />
+/// <reference path="filters/colorfilter.js" />
+/// <reference path="filters/colormatrix.js" />
+/// <reference path="filters/colormatrixfilter.js" />
+/// <reference path="filters/filter.js" />
+/// <reference path="lib/box2dweb-2.1.a.3.js" />
 /// <reference path="lib/easeljs-0.7.1.min.js" />
 /// <reference path="lib/jquery-2.1.1.js" />
 /// <reference path="lib/preloadjs-0.4.1.min.js" />
+/// <reference path="lib/r.js" />
+/// <reference path="lib/require.js" />
 /// <reference path="lib/soundjs-0.5.2.min.js" />
+/// <reference path="lib/stats.js" />
 /// <reference path="lib/tweenjs-0.5.1.min.js" />
-/// <reference path="lib/Box2dWeb-2.1.a.3.js" />
+<<<<<<< HEAD
+/// <reference path="lib/box2dweb-2.1.a.3.js" />
+=======
+/// <reference path="objects/Level.ts" />
+/// <reference path="keys.js" />
+/// <reference path="game.js" />
+/// <reference path="controls.js" />
+/// <reference path="config.js" />
+/// <reference path="box2d.js" />
+/// <reference path="managers/asset.js" />
+>>>>>>> origin/master
+/// <reference path="objects/coin.js" />
+/// <reference path="objects/enemy.js" />
+/// <reference path="objects/hero.js" />
+/// <reference path="objects/label.js" />
+<<<<<<< HEAD
+/// <reference path="objects/platform.js" />
+/// <reference path="objects/reality.js" />
+/// <reference path="objects/scoreboard.js" />
+/// <reference path="box2d.js" />
+=======
+/// <reference path="objects/Level.js" />
+/// <reference path="objects/platform.js" />
+/// <reference path="objects/reality.js" />
+/// <reference path="objects/scoreboard.js" />
+>>>>>>> origin/master
--- Commando/Scripts/game.js
+++ Commando/Scripts/game.js
@@ -1,37 +1,147 @@
-﻿/// <reference path="gameobjects.ts" />
+﻿/// <reference path="box2d.ts" />
+/// <reference path="config.ts" />
+/// <reference path="keys.ts" />
+/// <reference path="controls.ts" />
+/// <reference path="managers/asset.ts" />
+/// <reference path="objects/label.ts" />
+/// <reference path="objects/scoreboard.ts" />
+/// <reference path="objects/reality.ts" />
+/// <reference path="objects/platform.ts" />
+/// <reference path="objects/hero.ts" />
+/// <reference path="objects/enemy.ts" />
+/// <reference path="objects/coin.ts" />
+// Game Variables
+var arcadeCanvas;
+var debugCanvas;
+var stats;
 var stage;
-var queue;
+var ground;
+var level;
 
+<<<<<<< HEAD
 //Game Objects
 var firstLevel;
 var player;
 var skeleton;
+var ground;
+=======
+// Physics Variables
+var world;
+var reality;
 
-//Preload our assets
+// Game Objects
+var hero;
+var coin;
+var scoreboard;
+>>>>>>> origin/master
+
 function preload() {
-    Manager.Assets.init();
-    Manager.Assets.loader.addEventListener("complete", init);
+    managers.Assets.init();
+    managers.Assets.loader.addEventListener("complete", init);
 }
 
-//Init function, called when the body of the html page is loaded. Enables mouse over and ticker
 function init() {
-    State.MAINMENU = true;
-    stage = new createjs.Stage(document.getElementById("canvas"));
+    // Setup box2d reality
+    reality = new objects.Reality();
+    stage = new createjs.Stage(arcadeCanvas);
     stage.enableMouseOver(20);
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener("tick", gameLoop);
+    setupStats();
     gameStart();
 }
 
-// Game Loop, called every frame. Update game objects
 function gameLoop(event) {
+    // Start counting for FPS stats
+    if (config.SHOW_FPS) {
+        this.stats.begin();
+    }
+    hero.update();
+    if (controls.right == true) {
+        level.image.x -= 5;
+    }
+
+    //  scoreboard.update();
     stage.update();
+
+    reality.update();
+
+    // End counting for FPS stats
+    if (config.SHOW_FPS) {
+        return this.stats.end();
+    }
 }
 
-//Initialize objects on start.
+function setupStats() {
+    // Uses stats.js
+    stats = new Stats();
+    stats.setMode(0);
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.right = '0px';
+    stats.domElement.style.top = '0px';
+    document.body.appendChild(stats.domElement);
+}
+
 function gameStart() {
+<<<<<<< HEAD
+    ground = new objects.Platform(0, 20, 1024, 2);
+
+    stage.addChild(ground.view);
     firstLevel = new GameObjects.Level();
     player = new GameObjects.Player();
     skeleton = new GameObjects.Skeleton();
+=======
+    ground = new objects.Platform(0, 20.8, 500, 1);
+
+    //stage.addChild(ground.view);
+    var leftWall = new objects.Platform(0, 0, 1, 40);
+    stage.addChild(leftWall.view);
+
+    var rightWall = new objects.Platform(1020, 0, 1, 40);
+    stage.addChild(rightWall.view);
+
+    level = new objects.Level();
+
+    var platform1 = new objects.Platform(8, 16, 5, 1);
+    stage.addChild(platform1.view);
+
+    //var platform2 = new objects.Platform(11, 12, 5, 1);
+    //stage.addChild(platform2.view);
+    //var platform3 = new objects.Platform(14, 8, 5, 1);
+    //stage.addChild(platform3.view);
+    //var platform4 = new objects.Platform(22, 5, 9, 1);
+    //stage.addChild(platform4.view);
+    hero = new objects.Hero();
+    stage.addChild(hero.view);
+
+    //  scoreboard = new objects.Scoreboard();
+    var fixDef = new box2d.b2FixtureDef;
+    fixDef.density = 0.5;
+    fixDef.friction = 0.2;
+    fixDef.restitution = 0.5;
+
+    var bodyDef = new box2d.b2BodyDef;
+    var fixDefShape;
+    //create some objects
+    /*    bodyDef.type = box2d.b2Body.b2_dynamicBody;
+    for (var i = 0; i < 10; ++i) {
+    if (Math.random() > 0.5) {
+    fixDefShape = new box2d.b2PolygonShape();
+    fixDefShape.SetAsBox(
+    Math.random() + 0.1 //half width
+    , Math.random() + 0.1 //half height
+    );
+    fixDef.shape = fixDefShape;
+    } else {
+    fixDefShape = new box2d.b2CircleShape(
+    Math.random() + 0.1 //radius
+    );
+    fixDef.shape = fixDefShape;
+    }
+    bodyDef.position.x = Math.random() * stage.canvas.width / config.SCALE;
+    bodyDef.position.y = Math.random() * stage.canvas.height / config.SCALE;
+    world.CreateBody(bodyDef).CreateFixture(fixDef);
+    } */
+>>>>>>> origin/master
 }
 //# sourceMappingURL=game.js.map
